f9a3261a8f744e380ba1bd42cc0f1e7659e4e04d
fajari <m.soleh.fajari@gmail.com>
2022-11-10 16:28:13 +0700

    Update Comment

---
+++ data_syntax_perulangan.py
@@ -0,0 +1,6 @@
+"""
+Membaca buku
+"""
+
+total_read_book = 10
+# print("Jumlah Buku Awal=", total_read_book)
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 """
 This is your first python file
 """
-print("Helo World")
-print("My Name is John Clements")
+
+# Percabangan
+print("Budi diperintah ibunya untuk membeli Susu")
+print("Kalau ada telor, beli 6")
+
+# Pemrograman
+total_milk_onstore = 1
+total_egg_onstore = 4
+
+if total_milk_onstore > 0:
+    print("Budi membeli 1 susu")
+    if total_egg_onstore > 6:
+        print("Budi juga membeli 6 telor")
+    else:
+        print("Budi membeli", total_egg_onstore, "botol")
+        print(f"Jadi yang di beli Budi sejumlah {total_egg_onstore} telor dan {total_milk_onstore} susu")
+else:
+    print("Budi tidak membeli Susu")
+
+print("Budi pulang kerumah")
